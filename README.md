f91d9dc3fa3d6880a84309e75340f6625a7cc339
Alex <8208894+alexture@users.noreply.github.com>
2024-09-02 06:39:21 +0200

    slightly cleaner app, added credits to README so i have resources for later

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 This is a small project for a beginner. I'm hoping to create a simple website where you can filter country flags by whether they have red, white, or blue.
 
 Issues and contributions are welcome if you're patient and take the time to explain things, this is my first project! :)
+
+# Credits
+
+- Flag images: https://flagpedia.net/download
+- Methodology I'm going to follow (the link to their code is broken): https://time.com/patriotic-flag-colors/ 
--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+COLORS = [
+    "Red",
+    "White",
+    "Blue"
+]
+
+@app.route("/")
 def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-        return render_template("blue.html", name = name)
-    return render_template("index.html") # else is implicit since i've returned something
+    return render_template("index.html")#, colors=COLORS)
